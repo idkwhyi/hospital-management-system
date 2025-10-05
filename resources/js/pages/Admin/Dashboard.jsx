7520476fbf6968b9edac0b0f4a18d244ed39e61e
Endelif <92841501+idkwhyi@users.noreply.github.com>
2025-10-05 19:44:52 +0700

    React inertia setup and tailwind setup

---
+++ resources/js/pages/Admin/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Head } from '@inertiajs/react';
+
+export default function Dashboard() {
+    return (
+        <div className="p-4">
+            <Head title="Admin Dashboard" />
+            <h1 className='mt-10 text-red-200'>Admin Dashboard Page</h1>
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// routes/web.php
+use Inertia\Inertia;
+
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home');
+});
+
+Route::get('/admin/dashboard', function () {
+    return Inertia::render('Admin/Dashboard');
 });
